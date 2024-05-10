979d5c0dd74de372038f072bc412b5b2655264ac
thiago-shibata <thiago.shibatta@gmail.com>
2024-05-10 18:19:02 -0300

    remove middleware example

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -2,13 +2,8 @@ const { Router } = require("express")
 const UsersController = require("../controllers/UsersController")
 const userRouter = Router()
 
-function myMiddleware(request, response, next) {
-  console.log("você passou pelo middlware")
-  next()
-}
-
 const usersController = new UsersController()
 
-userRouter.post("/", myMiddleware, usersController.create)
+userRouter.post("/", usersController.create)
 
 module.exports = userRouter
