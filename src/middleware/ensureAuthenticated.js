22bb555f20bc304a33d6921803605ec90c7d9c78
thiago-shibata <thiago.shibatta@gmail.com>
2024-06-05 09:30:17 -0300

    add coments in lines

--- src/middleware/ensureAuthenticated.js
+++ src/middleware/ensureAuthenticated.js
@@ -1,19 +1,24 @@
 const { verify } = require("jsonwebtoken")
 const AppError = require("../utils/AppError")
-const authConfig = require("../configs/auth")
+const authConfig = require("../configs/auth") //arquivo de configurações de authenticação.
 
 function ensureAuthenticated(request, response, next) {
+  // informações do token
   const authHeader = request.headers.authorization
 
+  //verificando se existe um token
   if (!authHeader) {
     throw new AppError("JWT token não informado", 401)
   }
-
+  //atribuindo o atributo do array em uma variável token utilizando o split.
   const [, token] = authHeader.split(" ")
 
   try {
+    //veriricando se o token é válido
     const { sub: user_id } = verify(token, authConfig.jwt.secret)
 
+    //atribuindo a informação de ID contida no token em uma request user que pode ser acessada
+    //em outras partes da aplicação.
     request.user = {
       id: Number(user_id),
     }
