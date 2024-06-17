42affc0c9ce2ddbc57d994c3d605b211a61ed793
thiago-shibata <thiago.shibatta@gmail.com>
2024-06-17 13:47:27 -0300

    config uploads foders

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 require("express-async-errors")
 const migrationsRun = require("./database/sqlite/migrations")
 const AppError = require("./utils/AppError")
+const uploadConfig = require("./configs/upload")
 
 const express = require("express")
 const cors = require("cors")
@@ -10,8 +11,11 @@ const routes = require("./routes")
 migrationsRun()
 
 const app = express()
-app.use("cors")
+app.use(cors())
+
 app.use(express.json()) //config format request JSON
+
+app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER))
 app.use(routes)
 
 app.use((error, request, response, next) => {
